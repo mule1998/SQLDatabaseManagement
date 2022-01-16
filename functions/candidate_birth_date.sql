f33c0e77a1637a5d0c21e2fd085021e72f6efb1b
Shubham Mule <shubhambmule@gmail.com>
2022-01-16 20:13:44 +0530

    Added : functions

---
+++ functions/candidate_birth_date.sql
@@ -0,0 +1,21 @@
+use candidate_management;
+
+call company_name;
+
+
+DELIMITER ##
+CREATE FUNCTION DOB1(cand_id INT)
+returns date
+DETERMINISTIC
+BEGIN
+	DECLARE year1 date;
+		select birth_date into year1 from fellowship_candidate where id=cand_id;
+	return year1;
+
+end ##
+DELIMITER ;
+
+select DOB1(2);
+    
+
+
---
+++ functions/functions.sql
@@ -0,0 +1,71 @@
+use candidate_management;
+
+/*  Create a function to calculate age from DOB of candidate.  */
+
+DELIMITER ##
+CREATE FUNCTION candidate_age(cand_id INT)
+returns int
+DETERMINISTIC
+BEGIN
+	DECLARE age int;
+		SELECT (YEAR(CURDATE()) - YEAR(birth_date)) INTO age FROM fellowship_candidate WHERE id = cand_id;
+	RETURN age; 
+end ##
+DELIMITER ;
+
+drop function candidate_age;
+select candidate_age(2);
+
+
+/*  Get the full name of the candidate by candidate_id.  */
+
+DELIMITER ##
+CREATE FUNCTION candidate_name(cand_id INT)
+returns varchar(50)
+DETERMINISTIC
+BEGIN
+	DECLARE cand_name varchar(50);
+		SELECT concat(first_name,' ', IsNull(middle_name), ' ', last_name) into cand_name from fellowship_candidate where id = cand_id;
+	return cand_name;
+end ##
+DELIMITER ;
+
+drop function candidate_name;
+select candidate_name(2);
+
+
+/*   Take cpu percent and use case to print cpu percent is high when > 80,  normal<80 and else low.  */
+
+DELIMITER ##
+CREATE FUNCTION cpu_percent(cpu_prcnt double)
+returns varchar(50)
+DETERMINISTIC
+BEGIN
+	case
+		when cpu_prcnt > 80 then  return 'CPU Percent is High';
+		when cpu_prcnt < 80 then return 'CPU Percent is Normal';
+		else return 'LOW';
+	end case;
+end ##
+DELIMITER ;
+
+select cpu_percent(90);
+
+
+/*    Get company count by location.     */
+
+DELIMITER ##
+CREATE FUNCTION company_name(location_name VARCHAR(50))
+returns int
+DETERMINISTIC
+BEGIN
+	DECLARE count_company int;
+		select COUNT(location) into count_company from company where location = location_name;
+	RETURN count_company;
+end ##
+DELIMITER ;
+
+select company_name('PWC');
+    
+
+
