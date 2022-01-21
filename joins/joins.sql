15e7e1c9c8e5bea09596db158009b0c3ebfd9199
Shubham Mule <shubhambmule@gmail.com>
2022-01-21 22:17:43 +0530

    Added : joins

--- joins/joins.sql
+++ joins/joins.sql
@@ -1,23 +1,21 @@
-use candidate_management;
+/*Select mentor details and its tech name using inner join*/
 
-/*  Select mentor details and its tech name using inner join. */
+SELECT mentor.name, mentor.mentor_type, tech_stack.tech_name
+FROM mentor
+INNER JOIN mentor_techstack ON mentor.id = mentor_techstack.mentor_id
+INNER JOIN tech_stack ON mentor_techstack.tech_stack_id = tech_stack.id;
 
-SELECT mentor.*, ts.tech_name FROM mentor
-INNER JOIN
-	mentor_tech_stack AS mts ON mentor.id = mts.mentor_id
-INNER JOIN
-	tech_stack AS ts ON mts.tech_stack_id = ts.id;
-
-    
-/* select first name, last name ,password and contact number by doing outer join on candidate_fellowship and user_details table */
-
-SELECT fc.first_name, fc.last_name, ud.password, ud.contact_number FROM fellowship_candidate AS fc
-LEFT JOIN user_details AS ud ON fc.first_name = ud.first_name;
+/* select first name, last name ,password and contact number by doing 
+   outer join on candidate_fellowship and user_details table */
+   
+SELECT fellowship_candidates.first_name, fellowship_candidates.last_name ,user_details.contact_number, user_details.password
+FROM fellowship_candidates
+LEFT JOIN user_details ON fellowship_candidates.id = user_details.id;
 
 /* self join */
 
-SELECT A.first_name AS CandidateName1, B.first_name AS CandidateName2, A.hired_city
-FROM hired_candidate A,hired_candidate B
-WHERE A.id <> B.id
-AND A.hired_city = B.hired_city
-ORDER BY A.hired_city;
+SELECT b.first_name AS Name, b.mobile_num AS MobileNo, b.hired_city AS City, b.joining_date AS 'Joining Date'
+FROM fellowship_candidates a
+INNER JOIN fellowship_candidates b
+WHERE a.hired_city = b.hired_city AND b.first_name = 'Shivam';
+
